8b0a5e859748cf9fad4ed3f5501663cd93a5bbd5
hYdrago1 <vanyagoncha@gmail.com>
2025-07-23 16:49:28 +0200

    Initial Commit

---
+++ PersonalQwiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQwiz
+//
+//  Created by Ivan Honcharov on 23.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
